2a6a40bfac53983169aeda4dcd8b28c9c0a0457a
alana retamal <alana.retamal@segic.usach.cl>
2021-04-23 00:08:59 -0400

    Se cambia index

--- code/index.php
+++ code/index.php
@@ -1,42 +1,47 @@
 <?php
 
-//PDO
+// PDO
 require "util/db.php";
-$valido=0;
-if(isset($_POST['sign-up-button'])){
-	//Se envio el formulario
-	$name = $_POST["name"];
-	$email= $_POST["email"];
-	$username= $_POST["username"];
-	$pass= $_POST["pass"];
-	$repeatPass= $_POST['repeat-Pass'];
-	$rememberMe= $_POST['remember-Me'];
-	
+
+$valido = 0;
+
+if (isset($_POST['sign-up-button'])) {
+  	// Se envio el formulario
+  	$db = connectDB();
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$username = $_POST['username'];
+	$pass = $_POST['pass'];
+	$repeatPass = $_POST['repeat-pass'];
+	$rememberMe = $_POST['remember-me'];
+	$password = password_hash($pass, PASSWORD_DEFAULT);
+
 	$sql = "INSERT INTO users 
-		(full_name, email, user_name, password)
-		VALUES
-			(:full_name, :email, :user_name, :password)";
+				(full_name, email, user_name, password)
+			VALUES
+				(:full_name, :email, :user_name, :password)";
 
 	//statement
 	$stmt = $db->prepare($sql);
 
 	$stmt->bindParam(':full_name', $name);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':user_name', $username);
+	$stmt->bindParam(':email', $email);
+	$stmt->bindParam(':user_name', $username);
 	$stmt->bindParam(':password', password_hash($pass, PASSWORD_DEFAULT));
    
+
 	$stmt->execute();
 
-	$message = "Registro realizado con exito";
+	$message = "Registro realizado con éxito";
 	$valido = 1;
-}
-
+} 
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title> Registro Mentoria Web</title>
+	<title>Registro Mentoría WEB</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
@@ -63,13 +68,13 @@ if(isset($_POST['sign-up-button'])){
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 <!--===============================================================================================-->
-<style>
-.msg-form{
-	margin: 1em;
-	color:#66bb6a;
-}
-</style>
 
+	<style>
+		.msg-form {
+			margin: 1em;
+			color: #66bb6a;
+		}
+	</style>
 </head>
 <body style="background-color: #999999;">
 	
@@ -78,15 +83,16 @@ if(isset($_POST['sign-up-button'])){
 			<div class="login100-more" style="background-image: url('images/bg-01.jpg');"></div>
 
 			<div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
-				<form class="login100-form validate-form" method="GET" action="index.php">
+				<form class="login100-form validate-form" method="POST" action="register.php">
+					<input type="hidden" name="super-secreto" value="valor super secreto">
 					<span class="login100-form-title p-b-59">
 						Sign Up
 					</span>
-					
-					<?php if($valido == 1):?>
-					<p class="msg-form"><?= $message;?></p>
-					<?php endif;?>
- 
+
+					<?php if ($valido == 1): ?>
+						<p class="msg-form"><?= $message; ?></p>
+					<?php endif; ?>
+
 					<div class="wrap-input100 validate-input" data-validate="Name is required">
 						<span class="label-input100">Full Name</span>
 						<input class="input100" type="text" name="name" placeholder="Name...">
@@ -107,13 +113,13 @@ if(isset($_POST['sign-up-button'])){
 
 					<div class="wrap-input100 validate-input" data-validate = "Password is required">
 						<span class="label-input100">Password</span>
-						<input class="input100" type="password" name="pass" placeholder="*************">
+						<input class="input100" type="password" name="pass" placeholder="*****">
 						<span class="focus-input100"></span>
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Repeat Password is required">
 						<span class="label-input100">Repeat Password</span>
-						<input class="input100" type="password" name="repeat-pass" placeholder="*************">
+						<input class="input100" type="password" name="repeat-pass" placeholder="*****">
 						<span class="focus-input100"></span>
 					</div>
 
@@ -139,10 +145,10 @@ if(isset($_POST['sign-up-button'])){
 							<button class="login100-form-btn" name="sign-up-button">
 								Sign Up
 							</button>
-							<input type="hidden" name="super-secreto" value="valor secreto">
+
 						</div>
 
-						<a href="#" class="dis-block txt3 hov1 p-r-30 p-t-10 p-b-10 p-l-30">
+						<a href="index.php" class="dis-block txt3 hov1 p-r-30 p-t-10 p-b-10 p-l-30">
 							Sign in
 							<i class="fa fa-long-arrow-right m-l-5"></i>
 						</a>
