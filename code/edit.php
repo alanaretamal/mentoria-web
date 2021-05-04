b3c6e39c056be4b9d57696c88e204ddab0c657d1
root <root@ALANA.localdomain>
2021-05-04 08:39:25 -0400

    modificando todo

--- code/edit.php
+++ code/edit.php
@@ -1,16 +1,16 @@
 <?php
 session_start();
-if(!isset($_SESSION['nombre'])){
+if(!isset($_SESSION['name'])){
     header("location:index.php");
 }
 require "util/db.php";
 $valido = 0;
 if (isset($_GET['id'])) {
-    $idregistro = $_GET['id'];
+    $id = $_GET['id'];
     $db = connectDB();
 
     $sql = "SELECT id,full_name,user_name,email,password
-    FROM users where id=$idregistro";
+    FROM users where id=$id";
 
     //statement
     $stmt = $db->prepare($sql);
