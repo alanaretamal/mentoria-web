b1800e58099464b9c5f60f754dd547740fe51f5b
alana.retamal <alana.retamal@segic.usach.cl>
2021-05-25 11:02:26 -0400

    se implementa error 404

--- code/core/Router.php
+++ code/core/Router.php
@@ -1,76 +1,75 @@
-
 <?php
 
 namespace app\core;
 
-class Router{
-
+class Router
+{
     public Request $request;
     public Response $response;
-
     protected array $routes = [];
 
-    public function __construct (Request $request, Response $response){
+    public function __construct(\app\core\Request $request,Response $response)
+    {
         $this->request = $request;
         $this->response = $response;
     }
 
-    public function get($path, $callback){
+    public function get($path, $callback)
+    {
         $this->routes['get'][$path] = $callback;
     }
-
-    public function post($path, $callback){
+    public function post($path, $callback)
+    {
         $this->routes['post'][$path] = $callback;
     }
 
-    public function resolve(){
+    public function resolve()
+    {
         /*echo '<pre>';
         var_dump($_SERVER);
-        echo '</pre>';*/
+        echo '</pre>';
+        exit;*/
+
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
 
         $callback = $this->routes[$method][$path] ?? false;
-        //Application::$app;
-        /*var_dump($path);
-        var_dump($method);*/
-
-        if($callback == false){
-            //Application::$app->response->setStatusCode(404); es lo mismo q lo de abajo
-            $this->response->setStatusCode(404);
-            //return $this->renderContent("Not Found");
-            return $this->renderView("_404");
+        
+        if ($callback === false) {
+           // Application::$app->response->setStatusCode(404);
+           $this->response-> setStatusCode(404);
+           //return $this->renderContent("Not found");
+           return $this->renderView("_404");
+        
         }
-        //print_r($this->routes);
-
-        if (is_string($callback)){
+    
+        if(is_string(($callback)))
+        {
             return $this->renderView($callback);
         }
         return call_user_func($callback);
     }
-
-    public function renderContent($viewContent){
+    public function renderContent($viewContent)
+    {
         $layoutContent = $this->layoutContent();
-        $viewContent = $this->renderOnlyView($viewContent);
-        return str_replace('{{content}}', $viewContent, $layoutContent);
-    }    
-
-    public function renderView($view){
-        //interpolacion
+        return str_replace('{{content}}',$viewContent,$layoutContent);
+    }
+    public function renderView($view)
+    {
         $layoutContent = $this->layoutContent();
         $viewContent = $this->renderOnlyView($view);
-        return str_replace('{{content}}', $viewContent, $layoutContent);
+        return str_replace('{{content}}',$viewContent,$layoutContent);
     }
-
-    public function layoutContent(){
+    public function layoutContent()
+    {
         ob_start();
         include_once Application::$ROOT_DIR . "/views/layouts/main.php";
         return ob_get_clean();
-    }
 
-    public function renderOnlyView($view){
+    }
+    public function renderOnlyView($view)
+    {
         ob_start();
-        $layoutContent = $this->layoutContent();
         include_once Application::$ROOT_DIR . "/views/$view.php";
         return ob_get_clean();
     }
