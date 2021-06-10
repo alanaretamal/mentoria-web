762862966124ab6bf826808cf55d935ea69735e0
alana.retamal <alana.retamal@segic.usach.cl>
2021-06-10 11:02:11 -0400

    se implementa nueva validacion

--- code/core/Model.php
+++ code/core/Model.php
@@ -2,9 +2,12 @@
 
 namespace app\core;
 
-abstract class Model
-{
-    public const RULE_REQUIRED = 'required';
+// self hace referencias a la clase y solo se usa en la clase
+// this a los atributos de la clase, se puede utilizar en todos lados
+
+abstract class Model{
+
+    public const RULE_REQUIRED = 'requered';
     public const RULE_EMAIL = 'email';
     public const RULE_MIN = 'min';
     public const RULE_MAX = 'max';
@@ -12,10 +15,9 @@ abstract class Model
 
     public $errors = [];
 
-    public function loadData($data)
-    {
-        foreach ($data as $key => $value) {
-            if (property_exists($this, $key)) {
+    public function loadData($data){
+        foreach ($data as $key => $value){
+            if(property_exists($this, $key)){
                 $this->$key = $value;
             }
         }
@@ -23,50 +25,63 @@ abstract class Model
 
     abstract public function rules(): array;
 
-    public function validate()
-    {
-        foreach ($this->rules() as $attribute => $rules) {
-            $value = $this->$attribute;
-            foreach ($rules as $rule) {
+    public function validate(){
+        foreach ($this->rules() as $attribute => $rules){
+            $value = $this->$attribute; 
+            foreach($rules as $rule){
                 $rulename = $rule;
-
-                if (!is_string($rulename)) {
+                if (!is_string($rulename)){
                     $rulename = $rule[0];
                 }
 
-                if ($rulename === self::RULE_REQUIRED && !$value) {
+                if ($rulename === self::RULE_REQUIRED && !$value){
+                    // agregar Error
                     $this->addError($attribute, self::RULE_REQUIRED);
                 }
-                if ($rulename === self::RULE_MIN && strlen($value) < $rule['min']) {
+
+                if ($rulename === self::RULE_EMAIL && !filter_var($value, FILTER_VALIDATE_EMAIL)){
+                    $this->addError($attribute, self::RULE_EMAIL);
+                }
+                
+                if ($rulename === self::RULE_MIN && strlen($value) < $rule['min']){
                     $this->addError($attribute, self::RULE_MIN, $rule);
                 }
-                if ($rulename === self::RULE_MAX && strlen($value) > $rule['max']) {
+
+                if ($rulename === self::RULE_MAX && strlen($value) < $rule['max']){
                     $this->addError($attribute, self::RULE_MAX, $rule);
                 }
-                if ($rulename === self::RULE_MATCH && $value !== $this->{$rule['match']}) {
+
+                if ($rulename === self::RULE_MATCH && $value != $this->{$rule['match']}){
                     $this->addError($attribute, self::RULE_MATCH, $rule);
                 }
             }
         }
-
         return empty($this->errors);
     }
-
-    public function addError($attribute, $rule)
-    {
+    public function addError($attribute, $rule, $params = []){
         $message = $this->errorMessages()[$rule] ?? '';
 
+        foreach ($params as $key => $param){
+            $message= str_replace ("{{$key}}", $param, $message);
+        }
         $this->errors[$attribute][] = $message;
     }
 
-    public function errorMessages()
-    {
+    public function errorMessages(){
         return [
             self::RULE_REQUIRED => 'This field is required',
             self::RULE_EMAIL => 'This field must be an email',
             self::RULE_MIN => 'Min length of the field must be {min}',
             self::RULE_MAX => 'Max length of the field must be {max}',
-            self::RULE_MATCH => 'This fiels must be the same Password',
+            self::RULE_MATCH => 'This field must be the same as {match}',
         ];
+    }
+ 
+    public function hasError ($attribute){
+        return isset($this->errors[$attribute][0]);
+    }
+    
+    public function getFirstError ($attribute){
+        return $this->errors[$attribute][0] ?? false;
     }
 }
--- code/views/register.php
+++ code/views/register.php
@@ -1,25 +1,38 @@
-<h1>Register</h1>
+<h1>Register !!!</h1>
 
 <form method="POST">
   <div class="mb-3">
-    <label class="form-label">Firstname</label>
-    <input type="text" name="firstname" value="<?=$model->firstname?>"class="form-control">
+    <label class="form-label">FirstName</label>
+    <input type="text" name="firstName" value="<?= $model->firstName ?>" 
+    class="form-control <?= $model->hasError('firstName') ? 'is-invalid' : '' ?>">
+    <div class="invlaid-feedback">
+      <?= $model->getFirstError('firstName') ?>
+    </div>
   </div>
   <div class="mb-3">
-    <label class="form-label">Lastname</label>
-    <input type="text" name="lastname"  value="<?=$model->lastname?>"class="form-control">
+    <label class="form-label">LastName</label>
+    <input type="text" name="lastName" value="<?= $model->lastName ?>" 
+    class="form-control <?= $model->hasError('lastName') ? 'is-invalid' : '' ?>">
+    <div class="invlaid-feedback">
+      <?= $model->getFirstError('lastName') ?>
+    </div>
   </div>
   <div class="mb-3">
     <label class="form-label">Email</label>
-    <input type="text" name="email" class="form-control">
+    <input type="text" name="email" value="<?= $model->email ?>" 
+    class="form-control <?= $model->hasError('email') ? 'is-invalid' : '' ?>">
+    <div class="invlaid-feedback">
+      <?= $model->getFirstError('email') ?>
+    </div>
   </div>
   <div class="mb-3">
-    <label class="form-label">Password</label>  
+    <label class="form-label">Password</label>
     <input type="password" name="password" class="form-control">
   </div>
   <div class="mb-3">
-    <label class="form-label">Confirm Password</label>  
+    <label class="form-label">Confirm Password</label>
     <input type="password" name="confirmPassword" class="form-control">
   </div>
+
   <button type="submit" class="btn btn-primary">Save</button>
 </form>
