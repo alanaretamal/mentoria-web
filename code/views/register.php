05efc3d73e0f5e0984e36679fb35a54e8f8605dc
alana.retamal <alana.retamal@segic.usach.cl>
2021-06-11 11:26:41 -0400

    se agregan nuevas clases widgets

--- code/views/register.php
+++ code/views/register.php
@@ -1,38 +1,19 @@
 <h1>Register !!!</h1>
 
-<form method="POST">
-  <div class="mb-3">
-    <label class="form-label">FirstName</label>
-    <input type="text" name="firstName" value="<?= $model->firstName ?>" 
-    class="form-control <?= $model->hasError('firstName') ? 'is-invalid' : '' ?>">
-    <div class="invlaid-feedback">
-      <?= $model->getFirstError('firstName') ?>
-    </div>
-  </div>
-  <div class="mb-3">
-    <label class="form-label">LastName</label>
-    <input type="text" name="lastName" value="<?= $model->lastName ?>" 
-    class="form-control <?= $model->hasError('lastName') ? 'is-invalid' : '' ?>">
-    <div class="invlaid-feedback">
-      <?= $model->getFirstError('lastName') ?>
-    </div>
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Email</label>
-    <input type="text" name="email" value="<?= $model->email ?>" 
-    class="form-control <?= $model->hasError('email') ? 'is-invalid' : '' ?>">
-    <div class="invlaid-feedback">
-      <?= $model->getFirstError('email') ?>
-    </div>
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Password</label>
-    <input type="password" name="password" class="form-control">
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Confirm Password</label>
-    <input type="password" name="confirmPassword" class="form-control">
-  </div>
+<?php $form = \app\core\widgets\Form::begin('','POST') ?>
+<div class="row">
+<div class="col">
+<?= $form->field($model,'firstname') ?>
+</div>
+<div class="col">
+<?= $form->field($model,'lastname') ?>
+</div>
+</div>
 
-  <button type="submit" class="btn btn-primary">Save</button>
-</form>
+
+<?= $form->field($model,'email')->mailField() ?>
+<?= $form->field($model,'password')->passwordField() ?>
+<?= $form->field($model,'confirmPassword')->passwordField() ?>
+
+<button type="submit" class="btn btn-primary">Save</button>
+<?php \app\core\widgets\Form::end() ?>
