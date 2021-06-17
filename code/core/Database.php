7692c1d64038ce454aaefe1bb3aa54917673a735
alana.retamal <alana.retamal@segic.usach.cl>
2021-06-17 16:28:19 -0400

    Se modifica

--- code/core/Database.php
+++ code/core/Database.php
@@ -12,9 +12,9 @@ class Database{
         $username = $config['user'] ?? '';
         $password = $config['password'] ?? '';
 
-        //echo $dsn;
-        //echo $username;
-        //echo $password;
+        echo $dsn;
+        echo $username;
+        echo $password;
 
         $this->pdo = new \PDO($dsn, $username, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
@@ -22,45 +22,50 @@ class Database{
 
     public function applyMigrations(){
         $this->createMigrationsTable();
+        $newMigrations = [];
+        $appliendMigrations = $this->getAppliedMigrations();
+
+        $files = scandir(Application::$ROOT_DIR . '/migrations');
+        $toApplyMigrations = array_diff($files, $appliendMigrations);
         
-        $appliedMigrations = $this->getAppliedMigrations();
-       $files = scandir(Application::$ROOT_DIR . '/migrations');
-       $toApplyMigrations = array_diff($files, $appliedMigrations);
-       foreach($toApplyMigrations as $migration)
-       {
-           if($toApplyMigrations === '.' || $migration === '..'){
-               continue;
-           }
-           require_once Application::$ROOT_DIR . '/migrations/' . $migrations;
-          $className = pathinfo($migration,PATHINFO_FILENAME);
-          $instance = new $className();
-          echo "Applying migration $migration\n";
-          $instance->up();
-          echo "Applied migration $migration\n";
-          $newMigrations[] = $migratipn;
+        foreach ($toApplyMigrations as $migrations){
+            if ($migrations === '.' || $migrations === '..'){
+                continue;
+            }
+
+            require_once Application::$ROOT_DIR . '/migrations/' . $migrations; 
+            $className = pathinfo($migrations, PATHINFO_FILENAME);
+            $instance = new $className();
+            echo "Appliying migration $migrations \n";
+            $instance->up();
+            echo "Applied migration $migrations \n";
+
+            $newMigrations[] = $migrations;
         }
-        if(!empty($newMigrations))
-        {
+        if (!empty($newMigrations)){
             $this->saveMigrations($newMigrations);
-        }else{
-            echo "All migrations has been applied";
         }
+        else{
+            echo "All migrations has been applied \n";
+        }
+    
     }
-    public function createMigrationsTable()
-    {
-        $this->pdo->exec("CREATE TABLE IF NOT EXISTS `migrations` ( `id` INT NOT NULL AUTO_INCREMENT , `migration` VARCHAR(255) NOT NULL , `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+
+    public function createMigrationsTable(){
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS `migrations` ( `id` INT NOT NULL AUTO_INCREMENT , `migration` VARCHAR(255) NOT NULL , `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB; ");
     }
-    public function getAppliedMigrations()
-    {
+
+    public function getAppliedMigrations(){
         $sql = "SELECT migration FROM migrations";
-        $statement= $this->pdo->prepare($sql);
+        $statement = $this->pdo->prepare($sql);
         $statement->execute();
 
         return $statement->fetchAll(\PDO::FETCH_COLUMN);
     }
+
     public function saveMigrations(array $newMigrations){
-      
-        $values = implode(',',  array_map(fn($m) => "('$m')",$newMigrations));
+        
+        $values = implode(',', array_map(fn($m)=>"('$m')", $newMigrations));
         $statement = $this->pdo->prepare("INSERT INTO migrations (migration) VALUES $values");
         $statement->execute();
     }
