bb31da3bf471514a58f74a722297567cc8324852
alana.retamal <alana.retamal@segic.usach.cl>
2021-09-10 15:36:39 -0300

    Se agrega categoria

--- blog/resources/views/post.blade.php
+++ blog/resources/views/post.blade.php
@@ -1,13 +1,18 @@
-@extends('layout')
-@section('content')
-<p>
-        <a href="/category/{{$post->category->id}}" >
-                {{$post->category->name}}
-        </a>
-</p>
+<!-- codigo html cabecera hoja de estilo se llevo a la plantilla -->
+<!-- para que tome esa plantilla se debe hacer une extends de la plantilla padre -->
+<!-- luego se debe definir cual es el contenido que sera reemplazado en la plantilla con section -->
 
- <article>
-        <p>{{ $post->body }}</p>
+@extends("layout")
+@section("content")
+
+<article>
+<p> {{$post->body}} </p>
+
+<p> <a href="/category/{{$post->category->id}}">
+{{$post->category->name}}
+</a> </p>
+
+<p><?= $post->resumen ?></p>
 </article>
 <a href="/">Go Back</a>
-@endsection
+@endsection 
