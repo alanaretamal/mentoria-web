536513511baa62a26fc04f651ac8efbb28e9d84d
alana.retamal <alana.retamal@segic.usach.cl>
2021-09-16 10:44:58 -0300

    modificaciones

---
+++ blog/database/factories/PostFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    // Traits
+    use HasFactory;
+
+    //public $fillable = ['title', 'resumen', 'body'];
+    protected $guarded = ['id'];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    // hasOne, hasMany, belongsTo, belongsToMany
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
