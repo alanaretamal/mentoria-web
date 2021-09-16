d152663b142b224a04382942b56f3ead53a52503
alana.retamal <alana.retamal@segic.usach.cl>
2021-09-16 10:51:00 -0300

    modificaciones

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -10,21 +10,20 @@
         @foreach ($posts as $post)
             <article>
                 <h1>
-                    <a href="/post/<?= $post->slug ?>">
-                        {{ $post->title }} 
+                    <a href="/post/<?= $post->slug?>">
+                        {{ $post->title }}
                     </a>
                 </h1>
                 <p>
-                    By <a href="#">{{ $post->user->name }}</a> in 
-                    <a href="/category/{{$post->category->slug}}">
-                        {{$post->category->name}}
-                    </a>
+                    By <a href="#">{{$post->user->name}}</a> in
+                    <a href="/category/{{$post->category->slug}}" >
+                    {{$post->category->name}}
+                </a>
                 </p>
                 <p><?= $post->resumen ?></p>
             </article>
         @endforeach
     @else
         I don't have any posts!
-    @endif
-
+    @endif 
 @endsection
