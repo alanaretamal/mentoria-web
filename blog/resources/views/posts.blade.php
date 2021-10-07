3dce93af1e8f65253f36f77993198097eeb760c4
alana.retamal <alana.retamal@segic.usach.cl>
2021-10-07 04:52:01 -0300

    modificaciones

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -79,19 +79,26 @@
                     <div class="mt-8 flex flex-col justify-between">
                         <header>
                             <div class="space-x-2">
-                                <a href="/category/{{$post->category->slug }}"
+                                <a href="/category/{{ $post->category->slug }}"
                                    class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
                                    style="font-size: 10px">  {{ $post->category->name }}</a>
+
+                                <a href="#"
+                                   class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
+                                   style="font-size: 10px">Updates</a>
                             </div>
+
                             <div class="mt-4">
                                 <h1 class="text-3xl">
                                   {{ $post->title }}
                                 </h1>
+
                                 <span class="mt-2 block text-gray-400 text-xs">
                                         Published <time>1 day ago</time>
                                     </span>
                             </div>
                         </header>
+
                         <div class="text-sm mt-4">
                             <p>
                                 {{ $post->resumen }}   
