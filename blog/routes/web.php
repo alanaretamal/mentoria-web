75c26a0476361e1d66b8fb96a526563a2c12e345
alana.retamal <alana.retamal@segic.usach.cl>
2021-10-07 01:45:44 -0300

    modificaciones

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -20,7 +20,7 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 Route::get('/', function () {
     return view('posts', [
         'posts' => Post::latest('published_at')
-        ->with('category')
+        ->with(['category','user'])
         ->get()
     ]);
 });
