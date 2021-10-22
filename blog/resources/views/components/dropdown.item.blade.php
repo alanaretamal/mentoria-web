295d727f2f69cab38aceb9565e72ea283433c63a
alana.retamal <alana.retamal@segic.usach.cl>
2021-10-22 15:36:31 -0300

    modificaciones

---
+++ blog/resources/views/components/dropdown.item.blade.php
@@ -0,0 +1,11 @@
+@props(['actived'=> false])
+@php
+$classes= '"block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white ';
+
+if($actived){
+    $classes= 'bg-blue-500 hover:text-white';
+}
+@endphp
+<a {{$attributes(['class'=> $classes])}}>
+    {{ $slot }}
+</a>
