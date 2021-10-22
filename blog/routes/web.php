017200c9f4625c3dc9a70755dfea1b475adfb82f
alana.retamal <alana.retamal@segic.usach.cl>
2021-10-22 15:57:41 -0300

    modificaciones

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,25 +1,33 @@
-Route::get('/', function () {
-   //$posts = cache()->rememberForever('posts.all',fn () =>Post::all());
-    \Illuminate\Support\Facades\DB::listen(function( $query){
-        logger($query->sql , $query->bindings);
-    });
+<?php
 
-    
+use App\Models\Post;
+use App\Models\User;
+//use Illuminate\Support\Facades\File;
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+//use Spatie\YamlFrontMatter\YamlFrontMatter;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {   
     return view('posts', [
       'posts' => Post::latest('published_at')
       ->with(['category', 'author'])
       ->get() ,
        'categories' => Category::all()
-      //'post' => collect([])
-     ]);
+     
+     ])->name('home');
+});
 
-
-
-/*    $posts = Post::all();
-   return view('posts', [
-     'posts' => $posts
-    ]); */
-})->name('home');
 
 Route::get('/post/{post}', function( Post $post){
     return view ('post', [
