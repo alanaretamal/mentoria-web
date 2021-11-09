667395d71b53ec5061127cfa9adfd2f9b5a0f3d9
alana.retamal <alana.retamal@segic.usach.cl>
2021-11-09 15:41:36 -0300

    modificaciones

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -14,32 +14,52 @@ class Post extends Model
     //public $fillable = ['title', 'resumen' , 'body'];
 
     protected $guarded = ['id'];
-    public $with=['category','author'];
-    public function  getRouteKeyName(){
+    public $with = ['category', 'author'];
+    public function  getRouteKeyName()
+    {
         return 'slug';
     }
 
     //hasOne , hasMany , belongsTo , belongsToMany
-    public function scopeFilter($query,array $filters){
+    public function scopeFilter($query, array $filters)
+    {
 
         $query->when(
-        isset($filters['search']),
-        fn($query,$search)=>
-        $query->where('title','like',"%$search%")
-            ->orwhere('resumen','like',"%$search%")
+            isset($filters['search']) ?? false,
+            fn ($query, $search) =>
+            $query->where('title', 'like', "%$search%")
+                ->orwhere('resumen', 'like', "%$search%")
         );
+     /*    return $query->when(
+            isset($filters['category']) ?? false,
+            fn ($query, $search) =>
+            $query
+                ->whereExists(function($query){
+                  $query
+                   ->from('categories')
+                   ->whereColumn('categories.id','posts.category_id')
+                    ->where('categories.slug',$category);
+                })
+        ); */
 
-        if(isset($filters['search'])){
+        /*   if(isset($filters['search'])){
            return $query->where('title','like','%'.$filters('search').'%')
             ->orwhere('resumen','like','%'.$filters('search').'%');
-          }
+          } */
+          return $query->when(
+            $filters['category'] ?? false,
+            fn ($query, $category) =>
+            $query->whereHas('category',fn($query)=>
+            $query->where('slug',$category))
+          );
     }
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function author(){
-        return $this->belongsTo(User::class ,  'user_id' , 'id') ;
+    public function author()
+    {
+        return $this->belongsTo(User::class,  'user_id', 'id');
     }
-
 }
