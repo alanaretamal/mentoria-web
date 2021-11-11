dbf47a73ed25b298452ec97227be0682049e662d
alana.retamal <alana.retamal@segic.usach.cl>
2021-11-11 15:24:23 -0300

    modificaciones

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -8,16 +8,14 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
-    {    
-        return Post::latest('published_at')
-            ->filter(request(['search','category']))
-            ->paginate(5);
+    public function index(){
+   
+        return view('posts', [
+          'posts' => Post::latest('published_at')->filter( request(['search', 'category']))->paginate(6) ,
+           'categories' => Category::all(),
+           'currentCategory' => request('category') !== null ?  Category::where('slug', request('category'))->first() : null
+         ]);
 
-            /* 'categories' => Category::all(),
-            'currentCategory' => 
-              request('category')!==null ? Category::where('slug',request('category'))->first():null, 
-       ]); */
     }
     public function show(Post $post)
     {     
